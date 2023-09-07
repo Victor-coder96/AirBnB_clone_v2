a1861d53c08c985b28229ed4a11eb1cf952ef294
iamgideonchrist <attakpahgideon@gmail.com>
2023-09-07 10:58:27 -0700

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["54.237.119.247", "100.25.139.58"]
 env.user = "ubuntu"
 
 
