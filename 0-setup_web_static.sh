9126d5bd4c737baf64f601fb871d99aaed0feba4
victor-coder96 <vesionyeaba@gmail.com>
2023-10-08 02:52:06 -0700

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@ apt update -y
 apt install -y nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "<!DOCTYPE html>i
+echo "<!DOCTYPE html>
 <html>
   <head>
   </head>
@@ -13,6 +13,6 @@ echo "<!DOCTYPE html>i
   </body>
 </html>" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R www-data:www-data /data/
+chown -R ubuntu:ubuntu /data
 sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
